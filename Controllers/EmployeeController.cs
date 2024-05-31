6b4cf39db9da83f0d8032f943117896e252abaee
Akshat Nedungottil <akshatnedungottil@gmail.com>
2024-05-31 11:53:34 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using Common.DomainModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces;
+
+namespace EmployeeManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeServices _services;
+        public EmployeeController(IEmployeeServices employeeServices) 
+        { 
+        _services = employeeServices;
+        }
+
+        [HttpGet]
+        public List<Employee> GetEmployees() 
+        {
+            var employees = _services.GetAll();
+            return employees;
+        }
+    }
+}
